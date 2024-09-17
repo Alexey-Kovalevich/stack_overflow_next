e1bdf5cf2e0052891e70959a5c6e5a5c528d5bd8
Alexey Kovalevich <belshina25@gmail.com>
2024-09-17 15:35:11 +0300

    install additional packages

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div>
       <h1 className='h1-bold'>hey</h1>
+      <h2 className='h2-bold'>hey</h2>
+      <h3 className='h3-bold'>hey</h3>
     </div>
   );
 }
