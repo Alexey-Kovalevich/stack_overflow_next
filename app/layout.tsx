54a7556f726828f26f7a56d2deba2d5ba9486572
Alexey Kovalevich <belshina25@gmail.com>
2024-09-18 16:22:45 +0300

    implement ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,10 @@ import {
 } from '@clerk/nextjs'
 import './globals.css'
 // eslint-disable-next-line camelcase
-import {Inter, Space_Grotesk} from 'next/font/google'
+import { Inter, Space_Grotesk } from 'next/font/google'
 import React from 'react'
 import { Metadata } from 'next'
+import { ThemeProvider } from '@/context/ThemeProvider'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -37,26 +38,28 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 'primary-text-gradient hover:text-primary-500'
-        }
-      }}
-    >
-      <html lang="en">
-        <h1 className='h1-bold'>This is a text</h1>
-        <body className={`${inter.variable} ${spaceGrotest.variable}`}>
+
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotest.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: 'primary-gradient',
+              footerActionLink: 'primary-text-gradient hover:text-primary-500'
+            }
+          }}
+        >
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   )
 }
